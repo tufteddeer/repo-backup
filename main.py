951fd44ef9ec4dc47ca617bdd96250d20916c8c0
tufteddeer <tufteddeer@disroot.org>
2019-06-02 14:18:23 +0200

    error handling

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from github import Github
 import os
+import subprocess
 
+check = subprocess.Popen("git --version", shell=True)
+check.wait()
+if check.returncode != 0:
+    print("looks like the git executable is not in your path!")
+    exit(1)
 
-g = Github(os.environ['GITHUB_API_TOKEN'])
+try:
+    githubToken = os.environ['GITHUB_API_TOKEN']
+except KeyError:
+    print("Please set GITHUB_API_TOKEN environment variable!")
+    exit(1)
+
+g = Github(githubToken)
+
 dstDir = "repos"
 
 for repo in g.get_user().get_repos():
@@ -10,6 +23,12 @@ for repo in g.get_user().get_repos():
     print(repo.clone_url)
     dst = dstDir + "/" + repo.name
     if os.path.exists(dst):
-        os.system("cd " + dst + "&& git fetch")
+        fetch = subprocess.Popen("cd " + dst + "&& git fetch", shell=True)
+        fetch.wait()
+        if fetch.returncode != 0:
+            print("Failed to fetch " + repo.name)
     else:
-        os.system("git clone --mirror " + repo.clone_url + " " + dst)
+        clone = subprocess.Popen("git clone --mirror " + repo.clone_url + " " + dst, shell=True)
+        clone.wait()
+        if clone.returncode != 0:
+            print("Failed to clone " + repo.name)
